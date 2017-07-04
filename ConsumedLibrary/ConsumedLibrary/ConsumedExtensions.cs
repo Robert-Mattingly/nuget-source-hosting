3ad415d6fc5ee35634040a3da03140c40d79cb15
Robert-Mattingly <rbmattingly@gmail.com>
2017-07-04 14:20:02 -0400

    Created demo with direct referencing.

---
+++ ConsumedLibrary/ConsumedLibrary/ConsumedExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumedLibrary
+{
+    public static class ConsumedExtensions
+    {
+        public static string PadLength(this string baseString, char padChar, int totalLength)
+        {
+            var fullPad = Enumerable.Range(0, totalLength - baseString.Length).Select(x => padChar);
+            return baseString + new string(fullPad.ToArray());
+        }
+    }
+}
---
+++ ConsumedLibrary/ConsumingApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsumedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringsToPad = new string[]
+            {
+                "Carthage", "Rome", "Istanbul"
+            };
+
+            foreach (var @string in stringsToPad)
+                Console.WriteLine(@string.PadLength('*', 20));
+        }
+    }
+}
